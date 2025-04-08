6fea4f28aa222d6ecddd5d4c8b44836b6b0aa6fc
wellingtonPLF <wellplf@gmail.com>
2025-04-08 16:53:18 -0300

    Third commit

--- src/app/pages/HomePage/homeView.tsx
+++ src/app/pages/HomePage/homeView.tsx
@@ -4,6 +4,7 @@ const HomeView = () => {
   return (
       <>
           <div className="flex-center" id={styles.homePage}>HomePage</div>
+          <div className={`${styles['description']} bg-gray-500`}>description</div>
       </>
   );
 };
